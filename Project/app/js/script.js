0d1674b03f29a4cc5f850f0b9296daf2bb1a0dd8
Seryjnyy <61786337+Seryjnyy@users.noreply.github.com>
2021-03-16 17:13:22 +0000

    v2

--- Project/app/js/script.js
+++ Project/app/js/script.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+
+btnHamburger.addEventListener('click', () => 
+{
+    if(header.classList.contains('open')){ // Close
+        header.classList.remove('open');
+        body.classList.remove('noscroll');
+
+        fadeElems.forEach((value)=>{
+            value.classList.remove('fade-in');
+            value.classList.add('fade-out');
+        })
+    }else{
+        header.classList.add('open');     // Open
+        body.classList.add('noscroll');
+
+        fadeElems.forEach((value)=>{
+            value.classList.remove('fade-out');
+            value.classList.add('fade-in');
+        })
+    }
+    
+})
